9107a1f1b08e610905f375c5f9c2ff877316954b
usha devops <ushavislavath8742@gmail.com>
2019-05-18 04:42:43 -0400

    A.py file has been  created

---
+++ A.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python
+list=['abcd',736]
+print(list)
--- File1.java
+++ File1.java
@@ -1,11 +1,11 @@
-class A
+class File1
 {
   public static void main(String[] args)
   {
      int i;
      for(i=1;i<=10;i++)
      {
-       System.out.println(i)
+       System.out.println(i);
      }
     }
   }
